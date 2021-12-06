fa1c158c65309ca4fe177f60d7a910f7eb059fac
Siponek <szinkowicz@gmail.com>
2021-12-06 15:37:27 +0100

    Added funtions with write,read etc. TODO Fix write pipe

--- Assignment_ver_1.c
+++ Assignment_ver_1.c
@@ -54,8 +54,69 @@ void reset()
     fflush(stdout);
 }
 
+void createPipe(char *pipeName)
+{
+    if (mkfifo(pipeName, 0777) == -1)
+    {
+
+        if (errno != EEXIST)
+        {
+            printf("Could not create FIFO file: %d\n", errno);
+            exit(0);
+        }
+        else
+        {
+            printf("MKFIFO file already exists, running on...\n");
+        }
+    }
+    yellow();
+    printf("MKFIFO created\n");
+    reset();
+}
+
+int openPipe(char *pipeName)
+{
+    int fileDescriptor = open(pipeName, O_WRONLY);
+    if (fileDescriptor >= 0)
+    {
+        return fileDescriptor;
+    }
+    else
+    {
+        printf("Could not open FIFO file\n");
+        exit(0);
+    }
+}
+
+// void writeToPipe(char *pipe, char *moveSize)
+// {
+//     int fileDescriptor = openPipe(pipe);
+//     write(fileDescriptor, moveSize, strlen(moveSize) + 1);
+//     close(fileDescriptor);
+//     return NULL;
+// }
+
+void writeToPipe(char *motorSymbol, char *moveSize)
+{
+    // char pipeName[6];
+    // strcpy(&pipeName, 'PIPE_');
+    // strcat(&pipeName, &motorSymbol);
+
+    int fileDescriptor = openPipe(motorSymbol);
+    write(fileDescriptor, moveSize, strlen(moveSize) + 1);
+    close(fileDescriptor);
+}
+
+// THIS IS MAIN
 int main(int argc, char const *argv[])
 {
+    char *pipeX = "x";
+    char *pipeZ = "z";
+
+    createPipe(pipeX);
+    createPipe(pipeZ);
+
+    // FUNKCJE DO UŻYCIA - fd = open(), close(fd) write(fd, &do czego, sizeof()), read(fd, &do czego, sizeof())
     char userInput[100];
     while (1)
     {
@@ -70,7 +131,7 @@ int main(int argc, char const *argv[])
         if (isInputValid(userInput) == 1)
         {
             printf("Invalid input.");
-            exit(0);
+            continue;
         }
 
         char motorSymbol = userInput[0];
@@ -88,6 +149,25 @@ int main(int argc, char const *argv[])
 
         printf("motorSymbol -> %c\n", motorSymbol);
         printf("moveSize -> %s\n", moveSize);
+
+        if (userInput[0] == 'q')
+            exit(EXIT_SUCCESS);
+
+        writeToPipe(&motorSymbol, moveSize);
+
+        // if (motorSymbol == 'x')
+        // {
+        //     writeToPipe(pipeX, moveSize);
+        //     fileDescriptorX = openPipe(pipeX);
+        //     write(fileDescriptorX, moveSize, strlen(moveSize) + 1);
+        //     close(fileDescriptorX);
+        // }
+        // else if (motorSymbol == 'z')
+        // {
+        //     fileDescriptorZ = openPipe(pipeZ);
+        //     write(fileDescriptorZ, moveSize, strlen(moveSize) + 1);
+        //     close(fileDescriptorZ);
+        // }
     }
 
     /*fd[0] is for READ, fd[1] is for WRITE*/
